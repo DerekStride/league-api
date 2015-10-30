2dee9d9f4c41dd19cc308b3790f226aea0a5270c
JFleming4 <justinfleming95@gmail.com>
2015-10-30 12:31:00 -0400

    Can get the frequency of each champion played in ranked

--- stats.rb
+++ stats.rb
@@ -6,14 +6,41 @@ def api_key
   File.read(api_file_path).chomp
 end
 
-# pp api_key
-#
-# league_api = League.new(api_key)
-#
-# summoner = league_api.summoner_byname("SilentSith")
-#
-# sum = summoner[:silentsith][:id]
-#
-# matches = league_api.match_list(summoner[:silentsith][:id])
-#
-# pp matches
+def match_list_from_summoner_name(name, api)
+  name.downcase!
+  summoner = api.summoner_byname(name)
+  api.match_list(summoner.fetch(name.to_sym).fetch(:id))
+end
+
+def champion_key_from_id(champs, id)
+  champs.each {|champion|
+    if champion.fetch(:id) == id
+      return champion.fetch(:key)
+    end
+  }
+end
+
+def champions_played(match_list, champions)
+  played_champions = Hash.new(0)
+  # pp match_list
+  matches = match_list.fetch(:matches)
+  matches.each {|match|
+    played_champions[champion_key_from_id(champions, match.fetch(:champion)).to_sym] += 1
+  }
+  pp played_champions
+end
+
+def build_champ_hash(api)
+  champs = []
+  list = api.champions
+  list = list.fetch(:data)
+  list.each {|_key, val| champs << val}
+  champs
+end
+
+league_api = League.new(api_key)
+champions = build_champ_hash(league_api)
+puts "Enter Summoner Name"
+name = gets.chomp
+list = match_list_from_summoner_name(name, league_api)
+champions_played(list, champions)
