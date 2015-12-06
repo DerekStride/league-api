a999efe5b242eda262838c2f38f6cef8e364bf5f
DerekStride <djgstride@gmail.com>
2015-12-05 20:00:19 -0500

    Refactor the league class to use mixins and modules

--- league.rb
+++ league.rb
@@ -1,24 +1,84 @@
 require 'net/http'
 require 'json'
 
-# League interfaces with the riot API
-class League
-  attr_accessor :symbolize_json
+# LeagueAPICore contains the core functionality for the riot API
+module LeagueAPICore
+  private
 
-  def initialize(api_key, region = 'na')
-    @api = api_key
-    @region = region
-    @baseurl = "https://#{@region}.api.pvp.net"
-    @static_data_url = "#{@baseurl}/api/lol/static-data/#{@region}/v1.2"
-    @summoner_data_url = "#{@baseurl}/api/lol/#{@region}/v1.4/summoner"
-    @matchs_url = "#{@baseurl}/api/lol/#{@region}/v2.2/matchlist/by-summoner"
-    @stats_url = "#{@baseurl}/api/lol/#{@region}/v1.3/stats/by-summoner"
-    @symbolize_json = true
+  def query(uri, params = {})
+    uri.query = URI.encode_www_form(params.merge(api_key: @api))
+    response = nil
+    5.times do |_|
+      response = Net::HTTP.get_response(uri)
+      break unless %w(429 500).include?(response.code)
+      sleep(response['retry-after'].to_i || 2)
+    end
+    parse_response(response, symbolize_names: @symbolize_json)
   end
 
-  ########################################################################
-  # Static
-  ########################################################################
+  def parse_response(response, params = {})
+    bad_response(response)
+    result = JSON.parse(response.body, params)
+    # bad_request(result)
+    result
+  end
+
+  def bad_response(response)
+    puts 'ERROR:' \
+    "\nCode: #{response.code}" \
+    "\nBody: #{response.body}" unless response.is_a?(Net::HTTPSuccess)
+  end
+
+  def normalize_summoner_id(id)
+    return id if id.is_a? Fixnum
+    summoner_id_from_name(id)
+  end
+
+  def summoner_id_from_name(name)
+    id = name.downcase
+    summoner = summoner_byname(id)
+    if @symbolize_json
+      return summoner.fetch(id.to_sym).fetch(:id)
+    else
+      return summoner.fetch(id).fetch('id')
+    end
+  end
+end
+
+# LeagueAPIStaticMethods provides methods to get Static API Data
+module LeagueAPISummonerMethods
+  def summoner_byname(name)
+    path_val = URI.encode_www_form_component(name)
+    uri = URI("#{@summoner_data_url}/by-name/#{path_val}")
+    query(uri)
+  end
+
+  def summoner_by_id(id)
+    path_val = URI.encode_www_form_component(id)
+    uri = URI("#{@summoner_data_url}/#{path_val}")
+    query(uri)
+  end
+
+  def summoner_masteries(id)
+    uri = URI("#{@summoner_data_url}/#{id}/masteries")
+    query(uri)
+  end
+
+  def summoner_runes(id)
+    path_val = URI.encode_www_form_component(id)
+    uri = URI("#{@summoner_data_url}/#{path_val}/runes")
+    query(uri)
+  end
+
+  def summoner_name(id)
+    path_val = URI.encode_www_form_component(id)
+    uri = URI("#{@summoner_data_url}/#{path_val}/name")
+    query(uri)
+  end
+end
+
+# LeagueAPIStaticMethods provides methods to get Static API Data
+module LeagueAPIStaticMethods
   def match(match_id, params = {})
     path_val = URI.encode_www_form_component(match_id)
     uri = URI("#{@baseurl}/api/lol/na/v2.2/match/#{path_val}")
@@ -99,38 +159,13 @@ class League
     uri = URI("#{@static_data_url}/versions")
     query(uri)
   end
+end
 
-  ########################################################################
-  # Summoner
-  ########################################################################
-  def summoner_byname(name)
-    path_val = URI.encode_www_form_component(name)
-    uri = URI("#{@summoner_data_url}/by-name/#{path_val}")
-    query(uri)
-  end
-
-  def summoner_by_id(id)
-    path_val = URI.encode_www_form_component(id)
-    uri = URI("#{@summoner_data_url}/#{path_val}")
-    query(uri)
-  end
-
-  def summoner_masteries(id)
-    uri = URI("#{@summoner_data_url}/#{id}/masteries")
-    query(uri)
-  end
-
-  def summoner_runes(id)
-    path_val = URI.encode_www_form_component(id)
-    uri = URI("#{@summoner_data_url}/#{path_val}/runes")
-    query(uri)
-  end
-
-  def summoner_name(id)
-    path_val = URI.encode_www_form_component(id)
-    uri = URI("#{@summoner_data_url}/#{path_val}/name")
-    query(uri)
-  end
+# LeagueAPIMethods provides methods to talk to the Riot API, requires
+# implementing a query method to do the API calls
+module LeagueAPIMethods
+  include LeagueAPIStaticMethods
+  include LeagueAPISummonerMethods
 
   ########################################################################
   # Matchlist
@@ -151,45 +186,23 @@ class League
     uri = URI("#{@stats_url}/#{path_val}/ranked")
     query(uri, params)
   end
+end
 
-  private
+# League interfaces with the riot API
+class League
+  include LeagueAPICore
+  include LeagueAPIMethods
 
-  def query(uri, params = {})
-    uri.query = URI.encode_www_form(params.merge(api_key: @api))
-    response = nil
-    5.times do |_|
-      response = Net::HTTP.get_response(uri)
-      break unless %w(429 500).include?(response.code)
-      sleep(response['retry-after'].to_i || 2)
-    end
-    parse_response(response, symbolize_names: @symbolize_json)
-  end
+  attr_accessor :symbolize_json
 
-  def parse_response(response, params = {})
-    bad_response(response)
-    result = JSON.parse(response.body, params)
-    # bad_request(result)
-    result
-  end
-
-  def bad_response(response)
-    puts 'ERROR:' \
-    "\nCode: #{response.code}" \
-    "\nBody: #{response.body}" unless response.is_a?(Net::HTTPSuccess)
-  end
-
-  def normalize_summoner_id(id)
-    return id if id.is_a? Fixnum
-    summoner_id_from_name(id)
-  end
-
-  def summoner_id_from_name(name)
-    id = name.downcase
-    summoner = summoner_byname(id)
-    if @symbolize_json
-      return summoner.fetch(id.to_sym).fetch(:id)
-    else
-      return summoner.fetch(id).fetch('id')
-    end
+  def initialize(api_key, region = 'na')
+    @api = api_key
+    @region = region
+    @baseurl = "https://#{@region}.api.pvp.net"
+    @static_data_url = "#{@baseurl}/api/lol/static-data/#{@region}/v1.2"
+    @summoner_data_url = "#{@baseurl}/api/lol/#{@region}/v1.4/summoner"
+    @matchs_url = "#{@baseurl}/api/lol/#{@region}/v2.2/matchlist/by-summoner"
+    @stats_url = "#{@baseurl}/api/lol/#{@region}/v1.3/stats/by-summoner"
+    @symbolize_json = true
   end
 end
