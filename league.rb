a868938f01a97e82a8d5b8c2ddd5de58e609d1c0
DerekStride <djgstride@gmail.com>
2015-12-06 23:32:48 -0500

    Wrote and Implemented A simple JSON Caching Module

--- league.rb
+++ league.rb
@@ -1,25 +1,44 @@
 require 'net/http'
 require 'json'
+require_relative 'jsoncache'
 
 # LeagueAPICore contains the core functionality for the riot API
 module LeagueAPICore
+  include JSONCache
+
+  attr_accessor :symbolize_json, :retries
+
   private
 
-  def query(uri, params = {})
+  def get_response(uri, stale, delta, params = {})
+    json_params = { symbolize_names: @symbolize_json }
+    url = uri.to_s
+    return retrieve_cache(url, json_params) if cached?(url, stale, delta)
+
     uri.query = URI.encode_www_form(params.merge(api_key: @api))
-    response = nil
-    5.times do |_|
-      response = Net::HTTP.get_response(uri)
-      break unless %w(429 500).include?(response.code)
-      sleep(response['retry-after'].to_i || 2)
-    end
-    parse_response(response, symbolize_names: @symbolize_json)
+    query(uri)
   end
 
-  def parse_response(response, params = {})
+  def query(uri)
+    @retries.times do |_|
+      response = Net::HTTP.get_response(uri)
+      if %w(429 500 503).include?(response.code)
+        sleep(response['retry-after'].to_i || 2)
+      else
+        return parse_response(response, uri.to_s)
+      end
+    end
+  end
+
+  # Overwrite the JSONCache method to support cached data
+  def uri_to_file_path_root(uri)
+    uri.gsub(%r{[\.\/]|https:\/\/.*v\d\.\d|\?api_key=.*}, '')
+  end
+
+  def parse_response(response, uri)
     bad_response(response)
-    result = JSON.parse(response.body, params)
-    # bad_request(result)
+    result = JSON.parse(response.body, symbolize_names: @symbolize_json)
+    cache_file(result, uri) if response.is_a?(Net::HTTPSuccess)
     result
   end
 
@@ -35,7 +54,7 @@ module LeagueAPICore
   end
 
   def summoner_id_from_name(name)
-    id = name.downcase
+    id = name.chomp.delete(' ').downcase
     summoner = summoner_byname(id)
     if @symbolize_json
       return summoner.fetch(id.to_sym).fetch(:id)
@@ -50,30 +69,30 @@ module LeagueAPISummonerMethods
   def summoner_byname(name)
     path_val = URI.encode_www_form_component(name)
     uri = URI("#{@summoner_data_url}/by-name/#{path_val}")
-    query(uri)
+    get_response(uri, false, 0)
   end
 
   def summoner_by_id(id)
     path_val = URI.encode_www_form_component(id)
     uri = URI("#{@summoner_data_url}/#{path_val}")
-    query(uri)
+    get_response(uri, false, 0)
   end
 
   def summoner_masteries(id)
     uri = URI("#{@summoner_data_url}/#{id}/masteries")
-    query(uri)
+    get_response(uri, false, 0)
   end
 
   def summoner_runes(id)
     path_val = URI.encode_www_form_component(id)
     uri = URI("#{@summoner_data_url}/#{path_val}/runes")
-    query(uri)
+    get_response(uri, false, 0)
   end
 
   def summoner_name(id)
     path_val = URI.encode_www_form_component(id)
     uri = URI("#{@summoner_data_url}/#{path_val}/name")
-    query(uri)
+    get_response(uri, false, 0)
   end
 end
 
@@ -82,87 +101,87 @@ module LeagueAPIStaticMethods
   def match(match_id, params = {})
     path_val = URI.encode_www_form_component(match_id)
     uri = URI("#{@baseurl}/api/lol/na/v2.2/match/#{path_val}")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def champions(params = {})
     uri = URI("#{@static_data_url}/champion")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 
   def champion(champ_id, params = {})
     path_val = URI.encode_www_form_component(champ_id)
     uri = URI("#{@static_data_url}/champion/#{path_val}")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def items(params = {})
     uri = URI("#{@static_data_url}/item")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 
   def item(item_id, params = {})
     path_val = URI.encode_www_form_component(item_id)
     uri = URI("#{@static_data_url}/item/#{path_val}")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def masteries(params = {})
     uri = URI("#{@static_data_url}/mastery")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 
   def mastery(mastery_id, params = {})
     path_val = URI.encode_www_form_component(mastery_id)
     uri = URI("#{@static_data_url}/mastery/#{path_val}")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def runes(params = {})
     uri = URI("#{@static_data_url}/rune")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 
   def rune(rune_id, params = {})
     path_val = URI.encode_www_form_component(rune_id)
     uri = URI("#{@static_data_url}/rune/#{path_val}")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def summoner_spells(params = {})
     uri = URI("#{@static_data_url}/summoner-spell")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 
   def summoner_spell(summoner_spell_id, params = {})
     path_val = URI.encode_www_form_component(summoner_spell_id)
     uri = URI("#{@static_data_url}/summoner-spell/#{path_val}")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def languages
     uri = URI("#{@static_data_url}/languages")
-    query(uri)
+    get_response(uri, false, 0)
   end
 
   def map(params = {})
     uri = URI("#{@static_data_url}/map")
-    query(uri, params)
+    get_response(uri, false, 0, params)
   end
 
   def realm
     uri = URI("#{@static_data_url}/realm")
-    query(uri)
+    get_response(uri, false, 0)
   end
 
   def versions
     uri = URI("#{@static_data_url}/versions")
-    query(uri)
+    get_response(uri, false, 0)
   end
 end
 
 # LeagueAPIMethods provides methods to talk to the Riot API, requires
-# implementing a query method to do the API calls
+# implementing a get_response method to do the API calls
 module LeagueAPIMethods
   include LeagueAPIStaticMethods
   include LeagueAPISummonerMethods
@@ -174,7 +193,7 @@ module LeagueAPIMethods
     id = normalize_summoner_id(summoner_id)
     path_val = URI.encode_www_form_component(id)
     uri = URI("#{@matchs_url}/#{path_val}")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 
   ########################################################################
@@ -184,7 +203,7 @@ module LeagueAPIMethods
     id = normalize_summoner_id(summoner_id)
     path_val = URI.encode_www_form_component(id)
     uri = URI("#{@stats_url}/#{path_val}/ranked")
-    query(uri, params)
+    get_response(uri, true, 300, params)
   end
 end
 
@@ -192,8 +211,6 @@ end
 class League
   include LeagueAPICore
   include LeagueAPIMethods
-
-  attr_accessor :symbolize_json
 
   def initialize(api_key, region = 'na')
     @api = api_key
@@ -203,6 +220,8 @@ class League
     @summoner_data_url = "#{@baseurl}/api/lol/#{@region}/v1.4/summoner"
     @matchs_url = "#{@baseurl}/api/lol/#{@region}/v2.2/matchlist/by-summoner"
     @stats_url = "#{@baseurl}/api/lol/#{@region}/v1.3/stats/by-summoner"
+    @cache_directory = 'league'
     @symbolize_json = true
+    @retries = 5
   end
 end
